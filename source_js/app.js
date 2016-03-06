311561f7967349405ece502612368242ce8c8cbd
Alek Festekjian <festekj2@illinois.edu>
2016-03-06 16:25:41 -0600

    most functionality works except defaulting drop-down menu

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,23 @@
 var app = angular.module('mp3',['ngRoute']);
 
-app.config(function ($routeProvider) {
 
-})
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+    .when('/list', {
+        templateUrl : 'partials/list.html',
+        controller: 'listController'
+    })
+    .when('/details/:imdbID', {
+        templateUrl : 'partials/details.html',
+        controller: 'detailsController'
+    })
+    .when('/gallery', {
+        templateUrl : 'partials/gallery.html',
+        controller: 'galleryController'
+    })
+    .otherwise({
+        redirectTo: '/list'
+    });
+  }]);
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,38 @@
 /* Sample Controller */
+
+app.controller('galleryController', ['$scope', '$http',function($scope,$http) {
+    $http.get('./data/imdb250.json').
+    success(function(data, status, headers, config) {
+      $scope.movies = data;
+
+    }).
+    error(function(data, status, headers, config) {
+
+    });
+}]);
+
+app.controller('listController', ['$scope','$http',function($scope,$http,$routeParams) {
+  $http.get('./data/imdb250.json').
+  success(function(data, status, headers, config) {
+    $scope.movies = data;
+
+
+  }).
+  error(function(data, status, headers, config) {
+
+  });
+
+}]);
+
+app.controller('detailsController', ['$scope', '$http','$routeParams',function($scope,$http,$routeParams) {
+  $http.get('./data/imdb250.json').
+  success(function(data, status, headers, config) {
+    $scope.movies = data;
+
+  }).
+  error(function(data, status, headers, config) {
+
+  });
+  $scope.imdbID = $routeParams.imdbID;
+
+}]);
